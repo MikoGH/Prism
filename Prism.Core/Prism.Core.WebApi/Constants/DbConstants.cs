37a18a6cc65caec30be3b717ca408e335d9c94bf
Miko <unyperm@gmail.com>
2026-08-31 20:31:47 +0300

    feat: Add column names in db constants.

--- Prism.Core/Prism.Core.WebApi/Constants/DbConstants.cs
+++ Prism.Core/Prism.Core.WebApi/Constants/DbConstants.cs
@@ -29,7 +29,53 @@ public static class DbConstants
 
     public static class ColumnNames
     {
+        public const string Id = "id";
 
+        public const string UserId = "user_id";
+
+        public const string ThemeId = "theme_id";
+
+        public const string ThemeFieldId = "theme_field_id";
+
+        public const string RecordId = "record_id";
+
+        public const string RecordValueId = "record_value_id";
+
+        public const string RateId = "rate_id";
+
+        public const string Name = "name";
+
+        public const string ImageUrl = "image_url";
+
+        public const string Password = "password";
+
+        public const string Priority = "priority";
+
+        public const string Type = "type";
+
+        public const string Role = "role";
+
+        public const string Value = "value";
+
+        public const string Letter = "letter";
+
+        public const string Color = "color";
+
+        public const string WriteDate = "write_date";
+
+        public const string IsAccepted = "is_accepted";
+
+        public const string IsDeleted = "is_deleted";
+
+        public const string IntValue = "int_value";
+
+        public const string DoubleValue = "double_value";
+
+        public const string StringValue = "string_value";
+
+        public const string BoolValue = "bool_value";
+
+        public const string DateValue = "date_value";
     }
 
     public static class ColumnSettings
