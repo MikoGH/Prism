86e8d0c5db8551821052c07892cd89d5e82b9403
Miko <unyperm@gmail.com>
2026-09-01 17:51:25 +0300

    feat: Add Rate and User Dtos.

---
+++ Prism.Core/Prism.Core.WebApi/Dtos/Rate/RateDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace Prism.Core.WebApi.Dtos.Rate;
+
+public class RateDto
+{
+    public int Value { get; set; }
+
+    public char Letter { get; set; }
+
+    public Color Color { get; set; }
+}
---
+++ Prism.Core/Prism.Core.WebApi/Dtos/User/UserDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Prism.Core.WebApi.Dtos.User;
+
+public class UserDto
+{
+    public string Name { get; set; }
+}
